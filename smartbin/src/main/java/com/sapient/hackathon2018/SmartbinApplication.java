349ff16093ed42027d7300d254fbc8ed9eefebc8
Ashish Gupta <agupta191@sapient.com>
2018-05-05 10:19:47 +0530

    First Commit

---
+++ smartbin/src/main/java/com/sapient/hackathon2018/SmartbinApplication.java
@@ -0,0 +1,12 @@
+package com.sapient.hackathon2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbinApplication.class, args);
+	}
+}
